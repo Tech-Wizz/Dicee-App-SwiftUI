a0c7de4b24301b87b4ff779210eee5b5983d1d36
Kruize Derek Christensen <christensen.kruize@gmail.com>
2024-03-11 15:50:10 -0600

    Add files via upload

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Kruize Christensen on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
